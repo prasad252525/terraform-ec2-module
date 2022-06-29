21043bdba8453ffc6dd7bb5e874b41cdbe1221c6
prasad252525 <prasadseendrupu83@gmail.com>
2022-06-29 09:38:41 +0530

    prasad3

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+    type = string
+    default = null
+  
+}
+variable "instance_type" {
+    type = string
+    default = null
+  
+}
+variable "name" {
+    type = string
+    default = null
+  
+}
